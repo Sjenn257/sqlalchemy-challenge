3619d5eac97eb768e5b566746abeccbe4d32456e
Samantha Perez <sjenn257@gmail.com>
2021-09-08 20:29:12 -0500

    complete

--- app.py
+++ app.py
@@ -12,15 +12,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-    """List all available api routes."""
+    """Welcome page with available routes"""
+    
     return (
-        f"Available Routes:<br/>"
-        f"/api/v1.0/precipitation<br/>"
-        f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/tobs<br/>"
-        f'/api/v1.0/"start date"<br/>'        
-        f'/api/v1.0/"start date"/"end date"<br/><br/>'
-        f'**Must use date format in quotations: "yyyy-m-d"<br/>'
+        f"Welcome to this free API page for Honolulu, Hawaii's historical weather information including temperature and precipitation.<br/><br/>"
+
+        f"One-year information is available 2016-08-23 through 2017-08-23. Summary temperature data available 2010-01-01 through 2017-08-23.<br/><br/>"
+
+        f"Available Routes:<br/><br/>"
+
+        f"/api/v1.0/precipitation -- one year of precipitation data 2016-8-23 through 2017-08-23 at all Honolulu stations<br/>"
+        f"/api/v1.0/stations -- Honolulu stations and their locations<br/>"
+        f"/api/v1.0/tobs -- one year of temperatures observed at Honolulu's most active station 2016-8-23 through 2017-08-23<br/>"
+        f'/api/v1.0/starttemp/2016-01-01 -- provides high, low, average temperature from start date entered through 2017-08-23<br/>'        
+        f'/api/v1.0/starttemp/2016-01-01/endtemp/2016-01-31 -- provides high, low, average temperature from start date entered through the end date entered<br/><br/>'
+        f'*Enter date in this format: yyyy-mm-dd<br/>'
     )
 
 
@@ -36,7 +42,7 @@ def get_precipitation():
     session=Session(engine)
 
     year_start=dt.date(2017, 8 ,23)-dt.timedelta(days=365)
-    results = session.query(Measure.date, Measure.prcp).filter(Measure.date>=year_start).all()
+    results = session.query(Measure.date, Measure.prcp, Measure.station).filter(Measure.date>=year_start).all()
 
 
     session.close()
@@ -44,10 +50,11 @@ def get_precipitation():
 
     # Create a dictionary from the row data and append to a list of all_passengers
     all_precipitation = []
-    for date, prcp in results:
+    for date, prcp, station in results:
         prcp_dict = {}
         prcp_dict["date"] = date
         prcp_dict["prcp"] = prcp
+        prcp_dict["station"] = station
         all_precipitation.append(prcp_dict)
 
     return jsonify(all_precipitation)
@@ -64,7 +71,7 @@ def get_stations():
 
     session=Session(engine)
 
-    results=session.query(Station.station, Station.name).all()
+    results=session.query(Station.station, Station.name, Station.latitude, Station.longitude, Station.elevation).all()
 
 
     session.close()
@@ -72,10 +79,13 @@ def get_stations():
 
     # Create a dictionary from the row data and append to a list of all_passengers
     all_stations = []
-    for station, name in results:
+    for station, name, latitude, longitude, elevation in results:
         station_dict = {}
         station_dict["station"] = station
         station_dict["name"] = name
+        station_dict["latitude"] = latitude
+        station_dict["longitude"] = longitude
+        station_dict["elevation"] = elevation
         all_stations.append(station_dict)
 
     return jsonify(all_stations)
@@ -108,8 +118,8 @@ def get_tobs():
 
     return jsonify(all_tobs)
 
-@app.route("/api/v1.0/<start>")
-def get_start(start):
+@app.route("/api/v1.0/starttemp/<starttemp>")
+def get_start(starttemp):
     engine = create_engine("sqlite:///Resources/hawaii.sqlite",echo=False)
     Base = automap_base()
     Base.prepare(engine, reflect=True)
@@ -120,7 +130,7 @@ def get_start(start):
     session=Session(engine)
 
     results=session.query(func.min(Measure.tobs), func.max(Measure.tobs), func.avg(Measure.tobs)).\
-        filter(Measure.date>=start).all()
+        filter(Measure.date>=starttemp).all()
 
 
     session.close()
@@ -135,11 +145,13 @@ def get_start(start):
         summary_dict["avg"] = avg
         all_summary.append(summary_dict)
 
+    
+
     return jsonify(all_summary)
 
 
-@app.route("/api/v1.0/<start>/<end>")
-def get_end(start,end):
+@app.route("/api/v1.0/starttemp/<starttemp>/endtemp/<endtemp>")
+def get_end(starttemp,endtemp):
     engine = create_engine("sqlite:///Resources/hawaii.sqlite",echo=False)
     Base = automap_base()
     Base.prepare(engine, reflect=True)
@@ -150,7 +162,7 @@ def get_end(start,end):
     session=Session(engine)
 
     results=session.query(func.min(Measure.tobs), func.max(Measure.tobs), func.avg(Measure.tobs)).\
-        filter(Measure.date>=start).filter(Measure.date<=end).all()
+        filter(Measure.date>=starttemp).filter(Measure.date<=endtemp).all()
 
 
     session.close()
